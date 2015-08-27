64eb61d785cd9333b0d573e3e8abba9dd313b41f
nullet <tomnullet@gmail.com>
2015-08-27 14:27:29 -0500

    sessions controller and app controller updated (helper methods and basic auth)

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
